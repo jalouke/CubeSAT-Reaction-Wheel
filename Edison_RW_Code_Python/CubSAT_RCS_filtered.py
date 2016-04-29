a4ed1f619edbda73ca8372dfc3123502535f6ab4
Jeremy Louke <jalouke@gmail.com>
2016-04-29 13:57:39 -0700

    Adding Drop test data, updating cube code to latest versions and adding drop test code

--- Edison_RW_Code_Python/CubSAT_RCS_filtered.py
+++ Edison_RW_Code_Python/CubSAT_RCS_filtered.py
@@ -17,6 +17,32 @@ output = [A,B,C,D,E,F,G,H,I]
 Pin = [14,20,0,21,36,48,47,32,46] #PWM for GP13,GP12,GP182,GP183 Gpio for GP14,GP15,GP49,GP46,GP47
 
 ######################################################
+def butter_lowpass(cutoff, fs, order):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def manual_filt_low(b,a,data_in,data_out):
+	n = len(b)-1
+	#print n, data_in, b, a
+	out = b[0]*data_in[n]/a[0]
+	n-=1
+	for x in range(n+1):
+		out += b[x+1]*data_in[n]/a[0]
+		out -= a[x+1]*data_out[n]/a[0]
+		#print n,x+1, out, a[x+1], b[x+1]
+		n-=1
+	return out
+
+def floating_array_filter(b,a,in_window,out_window,in_temp):
+	in_window.append(in_temp)
+	in_window.pop(0)
+	out_temp = manual_filt_low(b,a,in_window,out_window)
+	out_window.append(out_temp)
+	out_window.pop(0)
+	return out_temp, out_window, in_window
+######################################################
 # enabling outputs 
 for x in xrange(0,4):
 	output[x] = mraa.Pwm(Pin[x])
@@ -30,6 +56,14 @@ for x in xrange(4,9):
 [Apwm,Bpwm,Cpwm,Dpwm,Adir,Bdir,Cdir,Ddir,mode] = output
 output[8].write(1) #Set mode pin to high for pwm/direction
 
+def gyroread():
+        c = 0
+        a = time.time()
+        while c <= timer:
+                [ACCx,ACCy,ACCz,GYRx,GYRy,GYRz,MAGx,MAGy,MAGz] = IMU.read()
+                print "GYRx: %3.2f, GYRy: %3.2f,GYRz: %3.2f" %(GYRx,GYRy,GYRz)
+                b = time.time()
+                c = b - a
 def A_motor_dir():
         global A_motor_speed
         if A_motor_velocity >= 0:
@@ -82,10 +116,13 @@ def D_motor_dir():
                  if D_motor_speed > 1:
                          D_motor_speed = 1
         Ddir.write(D_motor_dir)
-def roll_control():
+def roll_control(in_x_window,out_x_window,in_y_window,out_y_window,in_z_window,out_z_window):
 	t_a=time.time()
 	
+        global A_motor_velocity,B_motor_velocity,C_motor_velocity,D_motor_velocity
+        global ACCx,ACCy,ACCz,GYRx,GYRy,GYRz,MAGx,MAGy,MAGz
         [ACCx,ACCy,ACCz,GYRx,GYRy,GYRz,MAGx,MAGy,MAGz] = IMU.read()
+        GYRz, out_z_window, in_z_window = floating_array_filter(b,a,in_z_window,out_z_window,GYRz)
 	
         A_motor_velocity = P*GYRz + A_motor_velocity
         B_motor_velocity = P*GYRz + B_motor_velocity
@@ -101,11 +138,23 @@ def roll_control():
         Bpwm.write(B_motor_speed)
         Cpwm.write(C_motor_speed)
         Dpwm.write(D_motor_speed)
-
-	return [GYRz,A_motor_velocity,B_motor_velocity,C_motor_velocity,D_motor_velocity]
         
-while True:
-        [GYRz,A_motor_velocity,B_motor_velocity,C_motor_velocity,D_motor_velocity]=roll_control()
-        print "GYRz: %3.2f, A: %3.2f, B: %3.2f, C: %3.2f, D: %3.2f" %(GYRz,A_motor_velocity,B_motor_velocity,C_motor_velocity,D_motor_velocity)
+order = 2
+fs = 95       # sample rate, Hz
+cutoff = 15    # desired cutoff frequency of the filter, Hz
+b,a = butter_lowpass(cutoff, fs, order)
+in_x_window = [0.0]*(len(b))
+out_x_window = [0.0]*(len(a))
+in_y_window = [0.0]*(len(b))
+out_y_window = [0.0]*(len(a))
+in_z_window = [0.0]*(len(b))
+out_z_window = [0.0]*(len(a))
 
+
+while True:
+        roll_control(in_x_window,out_x_window,in_y_window,out_y_window,in_z_window,out_z_window)
+        print "GYRz: %3.2f, A: %3.2f, B: %3.2f, C: %3.2f, D: %3.2f" %(GYRz,A_motor_velocity,B_motor_velocity,C_motor_velocity,D_motor_velocity)
+       
+
+##        print "GYRx: %3.2f, GYRy: %3.2f,GYRz: %3.2f" %(GYRx,GYRy,GYRz)
         
--- Edison_RW_Code_Python/roll_test.py
+++ Edison_RW_Code_Python/roll_test.py
@@ -8,7 +8,7 @@ RAD_TO_DEG = 57.29578
 M_PI = 3.14159265358979323846
 Apwm=Bpwm=Cpwm=Dpwm=Adir=Bdir=Cdir=Ddir=mode=0 
 output = [Apwm,Bpwm,Cpwm,Dpwm,Adir,Bdir,Cdir,Ddir,mode] 
-Pin = [14,20,0,21,36,48,47,33,46] #PWM for GP13,GP12,GP182,GP183 Gpio for GP14,GP15,GP49,GP48,GP47
+Pin = [14,20,0,21,36,48,47,32,46] #PWM for GP13,GP12,GP182,GP183 Gpio for GP14,GP15,GP49,GP46,GP47
 
 for x in xrange(0,4):
 	output[x] = mraa.Pwm(Pin[x])
@@ -29,11 +29,9 @@ def rampup():
                 output[1].write(x)
                 output[2].write(x)
                 output[3].write(x)
-                time.sleep(.05)
+                time.sleep(.1)
                 x+=.05
-                [ACCx,ACCy,ACCz,GYRx,GYRy,GYRz,MAGx,MAGy,MAGz] = IMU.read()
-                print "GYRx: %3.2f, GYRy: %3.2f,GYRz: %3.2f" %(GYRx,GYRy,GYRz)
-        
+		print x        
 def rampdown():
         x=1
         while x >= 0:
@@ -41,95 +39,23 @@ def rampdown():
                 output[1].write(x)
                 output[2].write(x)
                 output[3].write(x)
-                time.sleep(.05)
+                time.sleep(.1)
                 x-=.05
-                [ACCx,ACCy,ACCz,GYRx,GYRy,GYRz,MAGx,MAGy,MAGz] = IMU.read()
-                print "GYRx: %3.2f, GYRy: %3.2f,GYRz: %3.2f" %(GYRx,GYRy,GYRz)
-def gyroread():
-        timer = 0
-        start = time.time()
-        while timer <= 1:
-                
-                [ACCx,ACCy,ACCz,GYRx,GYRy,GYRz,MAGx,MAGy,MAGz] = IMU.read()
-                print "GYRx: %3.2f, GYRy: %3.2f,GYRz: %3.2f" %(GYRx,GYRy,GYRz)
-                a = time.time()
-                timer = a - start
-                
-def jerk():
-        output[0].write(x)
-        output[1].write(x)
-        output[2].write(x)
-        output[3].write(x)
-        time.sleep(2)
-##        x=0
-##        output[0].write(x)
-##        output[1].write(x)
-##        output[2].write(x)
-##        output[3].write(x)
-##        time.sleep(.1)
-       
-
-        
+		print x
 
 while True:
         output[4].write(1)
         output[5].write(1)
         output[6].write(1)
         output[7].write(1)
+	print output[4]
         rampup()
-        gyroread()
         rampdown()
-        gyroread()
         output[4].write(0)
         output[5].write(0)
         output[6].write(0)
         output[7].write(0)
         rampup()
-        gyroread()
         rampdown()
-        gyroread()
-##        x=0
-##        ramp()
-##        output[4].write(1)
-##        output[5].write(1)
-##        output[6].write(0)
-##        output[7].write(0)
-##        x=1
-##        ramp()
-##        output[4].write(0)
-##        output[5].write(0)
-##        output[6].write(1)
-##        output[7].write(1)
-##        time.sleep(2)
-##        x=0
-##        ramp()
-##        output[4].write(0)
-##        output[5].write(1)
-##        output[6].write(1)
-##        output[7].write(0)
-##        x=1
-##        ramp()
-##        output[4].write(1)
-##        output[5].write(0)
-##        output[6].write(0)
-##        output[7].write(1)
-##        time.sleep(2)
-##        x=0
-##        ramp()
-##        a = time.time()
-##        [ACCx,ACCy,ACCz,GYRx,GYRy,GYRz,MAGx,MAGy,MAGz] = IMU.read()
-##        
-##        
-##        AccXangle =  (math.atan2(ACCy,ACCz)+M_PI)*RAD_TO_DEG
-##        AccYangle =  (math.atan2(ACCz,ACCx)+M_PI/2)*RAD_TO_DEG	
-##
-##        #Calculate heading
-##        heading = 180 * math.atan2(MAGy,MAGx)/M_PI
-##
-##        if heading < 0:
-##                heading += 360
-##        
-##        b = time.time()
-##        LoopTime = b - a
-##        print "GYRx: %3.2f, GYRy: %3.2f,GYRz: %3.2f" %(GYRx,GYRy,GYRz)
+
         
